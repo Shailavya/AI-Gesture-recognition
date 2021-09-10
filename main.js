3664cab1c34b4e4ef93885479cdffdf480283949
Shailavya <76102224+Shailavya@users.noreply.github.com>
2021-09-10 18:41:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
+ 
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
 
-    canvas = createCanvas(550, 500);
-    canvas.position(560, 150);
+    canvas = createCanvas(600, 450);
+    canvas.position(560, 80);
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on("pose", gotPoses);
 }
 
 function draw() {
+    document.getElementById("side_square").innerHTML = "Width and Height of the square will be - " + difference + " px";
     background("#009B81");
+    fill("#4682b4");
+    stroke("#4682b4");
+    square(noseX, noseY, difference);
 }
 
 function modelLoaded() {
@@ -20,5 +25,24 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results)
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX + " nose y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("Left wrist X = " + leftWristX + " Right wrist X = " + rightWristX +  "difference = " + difference);
+
     }
 }
+
+var noseX = 0;
+var noseY = 0;
+var difference = 0;
+var rightWristX = 0;
+var leftWristX = 0;
+
+
+
